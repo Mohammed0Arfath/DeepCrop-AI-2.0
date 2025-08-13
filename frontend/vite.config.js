3946a6346e4ef9965096634aa9aa5938287865d4
Hariharan.S <158813030+Hari20032005@users.noreply.github.com>
2025-08-13 10:28:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     host: true,
+    allowedHosts: [
+      'sugarcane-disease-detection-hn6m.onrender.com'
+    ],
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
